b05903a10442fa5680f7284055821499694852f4
Zahari.Kitanov@stp-online.de <Zahari.Kitanov@stp-online.de>
2022-09-28 16:41:52 +0300

    debugging changes

--- Program.cs
+++ Program.cs
@@ -6,9 +6,11 @@ namespace httpDownloader
     {
         static async Task Main(string[] args)
         {
+            var input = "-f urls.txt -o downloads -n 2 -l 100000";
+            var arguments = input.Split();
             //httpDownloader.exe -f urls.txt -o downloads -n 2 -l 100000
             var inputProcessor = new InputProcessorService();
-            await inputProcessor.ProcessInput(args);
+            await inputProcessor.ProcessInput(arguments);
         }
     }
 }
--- Services/FileService.cs
+++ Services/FileService.cs
@@ -22,6 +22,7 @@ namespace httpDownloader.Services
                         DownloadFile(item, settings.LocalStoragePath);
                     }).Start();
                 }
+                Console.WriteLine("=== Batch Finished ===");
             }
         }
 
